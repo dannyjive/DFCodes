9c29eb3436ba5cbcdb25f4c6f0afd828f9f6b046
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2026-01-14 17:10:05 -0800

    refined description text

--- script.js
+++ script.js
@@ -1,57 +1,65 @@
-      // --- MODAL ---
- 
-      document.addEventListener("DOMContentLoaded", function () {
-        const modalImage = document.getElementById("modalImage");
-        const modalTitle = document.getElementById("infoModalLabel");
-        const modalDescription = document.getElementById("modalDescription");
-        const modalLinks = document.getElementById("modalLinks");
+// --- MODAL ---
 
-        document.querySelectorAll(".open-modal").forEach((thumbnail) => {
-          thumbnail.addEventListener("click", function () {
-            modalImage.src = this.dataset.img;
-            modalImage.alt = this.alt;
-            modalTitle.textContent = this.dataset.title;
-            modalDescription.textContent = this.dataset.description;
+document.addEventListener("DOMContentLoaded", function () {
+  const modalImage = document.getElementById("modalImage");
+  const modalTitle = document.getElementById("infoModalLabel");
+  const modalDescription = document.getElementById("modalDescription");
+  const modalLinks = document.getElementById("modalLinks");
 
-            // Generate links
-            const links = JSON.parse(this.dataset.links);
-            modalLinks.innerHTML = links
-              .map(
-                (link) =>
-                  `<a href="${link.url}" class="btn btn-primary m-1" target="_blank">${link.text}</a>`
-              )
-              .join("");
-          });
-        });
+  document.querySelectorAll(".open-modal").forEach((thumbnail) => {
+    thumbnail.addEventListener("click", function () {
+      modalImage.src = this.dataset.img;
+      modalImage.alt = this.alt;
+      modalTitle.textContent = this.dataset.title;
+      modalDescription.textContent = this.dataset.description;
+
+      // Generate links
+      const links = JSON.parse(this.dataset.links);
+      modalLinks.innerHTML = links
+        .map(
+          (link) =>
+            `<a href="${link.url}" class="btn btn-primary m-1" target="_blank">${link.text}</a>`
+        )
+        .join("");
+    });
+  });
+});
+
+//Filter Buttons
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButtons = document.querySelectorAll(".filter-btn");
+  const filterItems = document.querySelectorAll(".filter-item");
+
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const filter = this.getAttribute("data-filter");
+
+      // Change active button styling
+      filterButtons.forEach((btn) => {
+        btn.classList.remove("active");
+        btn.classList.add("inactive");
       });
-     
-     //Filter Buttons
-      document.addEventListener("DOMContentLoaded", function () {
-        const filterButtons = document.querySelectorAll(".filter-btn");
-        const filterItems = document.querySelectorAll(".filter-item");
 
-        filterButtons.forEach((button) => {
-          button.addEventListener("click", function () {
-            const filter = this.getAttribute("data-filter");
+      this.classList.add("active");
 
-            // Change active button styling
-            filterButtons.forEach((btn) => {
-              btn.classList.remove("active");
-              btn.classList.add("inactive");
-            });
+      // Show/Hide items based on filter
 
-            this.classList.add("active");
+      filterItems.forEach((item) => {
+        const categories = item.getAttribute("data-category").split(" ");
 
-            // Show/Hide items based on filter
-            filterItems.forEach((item) => {
-              const categories = item.getAttribute("data-category").split(" "); // Split categories into array
-
-              if (filter === "all" || categories.includes(filter)) {
-                item.style.display = "block"; // Show matching items
-              } else {
-                item.style.display = "none"; // Hide non-matching items
-              }
-            });
-          });
-        });
+        if (filter === "all" || categories.includes(filter)) {
+          item.classList.remove("d-none");
+        } else {
+          item.classList.add("d-none");
+        }
       });
+    });
+  });
+});
+
+// <!-- Current Year Script -->
+
+const year = new Date().getFullYear();
+document.getElementById(
+  "copyright"
+).innerHTML = `This website was designed and coded by Dan Finley &copy; ${year}`;
